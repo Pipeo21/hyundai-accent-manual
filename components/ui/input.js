38334506fa0176a5b5fc98239590534e55ca38b5
Pipeo21 <felipeosorio.21@gmail.com>
2025-07-17 12:25:10 -0400

    Delete components/ui directory

--- components/ui/input.js
+++
@@ -1,3 +0,0 @@
-export function Input({ value, onChange, placeholder, className }) {
-  return <input value={value} onChange={onChange} placeholder={placeholder} className={`border p-2 ${className}`} />;
-}
